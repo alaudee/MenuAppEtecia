72db7cb820ec79fd936e68d2e01d0276bb6cbdb1
christopher <christopher.ferreira.silva@outlook.com>
2019-05-08 22:19:18 -0300

    Criado o aplicativo com menu

---
+++ app/src/main/java/br/com/etecia/menuappetecia/PesquisarActivity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.menuappetecia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PesquisarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pesquisar_layout);
+    }
+}
